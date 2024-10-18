4b419d25193f31b78974c6c8152e570b6e219bae
jduke788 <jduke788@gmail.com>
2024-10-18 16:25:29 +0100

    all works except genre

--- routes/show.js
+++ routes/show.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const Show = require("../models/Show")
+const User = require("../models/User")
 const router = express.Router()
 
 
@@ -8,5 +9,44 @@ router.get("/", async (req, res) => {
      res.json({shows})
  })
 
+ router.get("/:id", async (req,res) => {
+    const show = await Show.findByPk(req.params.id)
+    res.json(show)
+})
+
+router.get("/:id/users", async (req, res) => {
+    const showUser = await Show.findByPk(req.params.id, { include: [{model: User, as: "users"}]})
+    res.json(showUser.users)
+})
+
+router.put("/:id/available", async (req, res) => {
+    const show = await Show.findByPk(req.params.id)
+    if (show) {
+        show.available = req.body.available;
+        await show.save();
+        res.json(show);
+      } else {
+        res.status(404).json({ error: 'Show not found' })
+}})
+
+
+router.delete("/:id", async (req, res) => {
+    const deleteShow = await Show.destroy({where: {id: req.params.id}})
+    res.json(deleteShow)
+})
+
+router.get('/:genre', async (req, res) => {
+    try {
+      const genre = req.params.genre;
+  
+      const shows = await Show.findAll({
+        where: { genre: genre }
+      });
+  
+      res.json(shows);
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  });
 
  module.exports = router
--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const User = require("../models/User")
+const { Show } = require("../models")
 const router = express.Router()
 
 
@@ -9,6 +10,29 @@ router.get("/", async (req, res) => {
     const users = await User.findAll()
     res.json(users)
 })
+
+router.get("/:id", async (req,res) => {
+    const user = await User.findByPk(req.params.id)
+    res.json(user)
+})
+
+router.get("/:id/shows", async (req, res) => {
+    const userShow = await User.findByPk(req.params.id, { include: [{model: Show, as: "shows"}]})
+    res.json(userShow.shows)
+})
+
+router.put("/:userId/shows/:showId", async (req, res) => {
+    const user = await User.findByPk(req.params.userId)
+    const show = await Show.findByPk(req.params.showId)
+    if (user && show) {
+        show.userId = user.id
+        await show.save()
+        res.json(show)
+    } else {
+        res.status(404).json({error: 'User and/or show not found'})
+    }
+})
+
 
 
 
